158cb0ae2210533a60834a0bbc8c4a33328f9dec
Anne Livia <annelivia16@gmail.com>
2022-03-14 17:29:06 -0300

    ticket store

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -5,7 +5,16 @@ import Controller from './Controller.js';
 import { BadRequest, Forbidden } from '../utils/CustomError.js';
 
 const Joi = joiImported.extend(joiDate);
-const { SessionRoom } = Prisma;
+const { SessionRoom, SeatStatus, SeatType } = Prisma;
+
+const schema = Joi.object({
+  // HH = hours (24 hours time). if hh (12 hours time with a or A where a = am and A = pm)
+  sessionDate: Joi.date().required().format('DD/MM/YYYY HH:mm'),
+  room: Joi.string().valid(...Object.values(SessionRoom)),
+  // Requires the string value to be a valid GUID.
+  movieId: Joi.string().required().guid(),
+  price: Joi.number().required().positive(),
+});
 
 class Session extends Controller {
   maxLines = 2;
@@ -21,15 +30,15 @@ class Session extends Controller {
 
   constructor() {
     super('session', {
-      findMany: { include: { movie: true, sessionSeats: true } },
+      findMany: { include: { movie: true, SessionSeat: true } },
     });
   }
 
   generateSeats() {
-    // an array of SessionSeats objects to create many when creating a session
+    // an array of SessionSeat objects to create many when creating a session
     const seats = [];
 
-    // in sessionseats model, sessionId, is automatically inserted since I'm doing Nested writes
+    // in sessionseat model, sessionId, is automatically inserted since I'm doing Nested writes
     // in order to use ++ or -- unary. https://eslint.org/docs/rules/no-plusplus
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < this.maxLines; i++) {
@@ -43,8 +52,7 @@ class Session extends Controller {
             column,
             name: `${line}${column}`,
             type: 'STARDARD',
-            state: 'AVAILABLE',
-            disable: false,
+            status: 'AVAILABLE',
           },
         );
       }
@@ -57,21 +65,7 @@ class Session extends Controller {
   async store(req, res, next) {
     if (req.userLogged.role !== 'ADMIN') { throw new Forbidden('You don\'t have Admin privileges'); }
 
-    const Schema = Joi.object({
-      // HH = hours (24 hours time). if hh (12 hours time with a or A where a = am and A = pm)
-      sessionDate: Joi.date().required().format('DD/MM/YYYY HH:mm'),
-      room: Joi.string().valid(
-        SessionRoom.COMMON,
-        SessionRoom.DLUX,
-        SessionRoom.IMAX,
-        SessionRoom.XD,
-      ),
-      // Requires the string value to be a valid GUID.
-      movieId: Joi.string().required().guid(),
-      price: Joi.number().required().positive(),
-    });
-
-    const { error } = Schema.validate(req.body, { abortEarly: false });
+    const { error } = schema.validate(req.body, { abortEarly: false });
 
     if (error) {
       console.error(error);
@@ -93,7 +87,7 @@ class Session extends Controller {
       movie: { connect: { id: movieId } },
       price,
       // create many options, to generate seats associated to this session.
-      SessionSeats: { createMany: { data: this.generateSeats() } },
+      SessionSeat: { createMany: { data: this.generateSeats() } },
     };
 
     super.store(req, res, next);
@@ -101,27 +95,31 @@ class Session extends Controller {
 
   // only admin user can update
   async update(req, res, next) {
+    const {
+      room, sessionDate, price, movieId,
+    } = req.body;
+
     if (req.userLogged.role !== 'ADMIN') { throw new Forbidden('You don\'t have Admin privileges'); }
 
-    const Schema = Joi.object({
-      // HH = hours (24 hours time). if hh (12 hours time with a or A where a = am and A = pm)
-      sessionDate: Joi.date().format('DD/MM/YYYY HH:mm'),
-      room: Joi.string().valid(
-        SessionRoom.COMMON,
-        SessionRoom.DLUX,
-        SessionRoom.IMAX,
-        SessionRoom.XD,
-      ),
-      // Requires the string value to be a valid GUID.
-      movieId: Joi.string().guid(),
-    });
-
-    const { error } = Schema.validate(req.body, { abortEarly: false });
+    const { error } = schema.validate(
+      {
+        room, sessionDate, price, movieId,
+      },
+      { abortEarly: false },
+    );
 
     if (error) {
       console.error(error);
       throw new BadRequest('Invalid data', error.details.map(({ message }) => message));
     }
+
+    // inserting reqbody and updating
+    req.body = {
+      room,
+      sessionDate,
+      price,
+      movie: { connect: { id: movieId } },
+    };
 
     super.update(req, res, next);
   }
@@ -131,6 +129,36 @@ class Session extends Controller {
     if (req.userLogged.role === 'ADMIN') { return super.remove(req, res, next); }
 
     throw new Forbidden('You don\'t have Admin privileges');
+  }
+
+  async updateSeat(req, res) {
+    const { idSession, idSeat } = req.params;
+
+    const { status, type } = req.body;
+
+    const SeatSchema = Joi.object({
+      status: Joi.string().valid(...Object.values(SeatStatus)),
+      type: Joi.string().valid(...Object.values(SeatType)),
+    });
+
+    const { error } = SeatSchema.validate({ status, type }, { abortEarly: false });
+
+    if (error) {
+      throw new BadRequest('Invalid data', error.details.map(({ message }) => message));
+    }
+
+    const sessionSeat = await this.prismaClient.session.update(
+      {
+        where: { id: idSession },
+        data: {
+          SessionSeat: {
+            update: { data: { type, status }, where: { id: idSeat } },
+          },
+        },
+      },
+    );
+
+    res.json(sessionSeat.SessionSeat);
   }
 }
 
